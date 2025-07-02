26b5e29aafd8a5b9a47f494fc1913a4d733bff43
nicoegiaian <nicolas.egiaian@gmail.com>
2025-07-02 12:33:42 -0300

    Correccion next config y package

--- next.config.js
+++ next.config.js
@@ -1,18 +1,30 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
+  webpack: (config, { dev, isServer }) => {
+    // Configuración específica para Windows - case sensitivity fix
+    if (dev && !isServer) {
       config.watchOptions = {
         poll: 1000,
         aggregateTimeout: 300,
       }
+      
+      // Forzar case sensitivity en webpack
+      config.resolve.plugins = config.resolve.plugins || []
+      config.resolve.plugins.push(
+        new (require('webpack/lib/CaseSensitivePathsPlugin'))()
+      )
     }
+    
     return config
   },
-  // Desactivar optimizaciones problemáticas en desarrollo
+  
+  // Configuraciones adicionales para estabilidad
   swcMinify: true,
-  experimental: {
-    forceSwcTransforms: true,
+  
+  // Variables de entorno para desarrollo
+  env: {
+    FORCE_COLOR: '1'
   }
 }
 
